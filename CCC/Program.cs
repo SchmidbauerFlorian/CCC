f34c9f4a9b7451c7408fa73e80ce8f3649df3bb8
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2024-04-19 10:00:11 +0200

    supa

--- CCC/Program.cs
+++ CCC/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        public static StreamReader SR { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+
+
+    public class Extractor
+    {
+
+        public static List<string> Extract(string path, string sep = " ", int startToRead = 0)
+        {
+            SR = new StreamReader(path);
+            List<string> strings = new List<string>();
+            int counter = 0;
+            while (!SR.EndOfStream)
+            {
+                if (counter >= startToRead)
+                    strings.AddRange(SR.ReadLine().Trim().Split(sep));
+                else
+                    SR.ReadLine();
+                counter++;
+            }
+            return strings;
         }
     }
 }
